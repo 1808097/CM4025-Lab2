e915bdfca2a3ffa4f1304409135da97cd529b5bb
ANDRAS SARDI (1808097) <a.sardi@rgu.ac.uk>
2022-01-25 16:35:58 +0000

    Adding Admin Route

--- Lab2/server.js
+++ Lab2/server.js
@@ -8,5 +8,18 @@ app.get('/', function(req, res) {
  res.sendFile(__dirname + '/index.html');
 });
 
+var adminRouter = express.Router();
+
+adminRouter.get('/', function(req, res) {
+ res.send('I am the dashboard!'); });
+
+adminRouter.get('/users', function(req, res) {
+ res.send('I show all the users!'); });
+
+adminRouter.get('/posts', function(req, res) {
+ res.send('I show all the posts!'); });
+
+app.use('/admin', adminRouter);
+
 app.listen(PORT);
 console.log('Express Server running at http://127.0.0.1:'.PORT);
